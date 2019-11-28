efec59e26d317ea55c677cbcad0bd07bec59f700
berezhnayaalyona <berezhnayaa2015@mail.ru>
2019-11-28 14:40:40 +0300

    insertionSort

--- cod1.cpp
+++ cod1.cpp
@@ -119,3 +119,98 @@ public:
 		else cout << endl << " Устанем ждать " << endl << endl;
 
 	}
+	void bubbleSort() {
+
+		cout << " \t\tСортировка пузырьком\t\t\n " << endl;
+
+		if (SIZE <= 49999) {
+
+			int* arrBubble = new int[SIZE];
+
+
+			for (int i = 0; i < SIZE; ++i)	arrBubble[i] = arr[i];
+
+
+			for (int i = SIZE - 1; i > 0; --i, ++count) {
+
+				for (int j = 0; j < i; ++j, ++count) {
+
+					if (arrBubble[j] > arrBubble[j + 1] && ++count) {
+
+						swap(arrBubble[j], arrBubble[j + 1]);
+						++count;
+
+					}
+
+				}
+
+			}
+
+
+			arrTime[0] = count;
+
+			if (SIZE <= 1000) {
+
+				cout << "   Отсортированный массив : ";
+
+				for (int i = 0; i < SIZE; ++i) cout << arrBubble[i] << " ";
+
+			}
+
+			cout << endl << " Время работы сортировки : " << count;
+
+			count = 0;
+
+			cout << endl << endl;
+
+			delete[]arrBubble;
+
+		}
+
+		else cout << endl << " Устанем ждать " << endl << endl;
+
+	}
+
+	void insertinSort() {
+
+		cout << " \t\tСортировка вставками\t\t\n " << endl;
+
+		if (SIZE <= 49999) {
+
+			int* arrInsertion = new int[SIZE];
+
+			for (int i = 0; i < SIZE; ++i) arrInsertion[i] = arr[i];
+
+
+			for (int i = 1; i < SIZE; ++i, ++count)
+				for (int j = i; j > 0 && arrInsertion[j - 1] > arrInsertion[j] && ++count; --j, ++count) {
+
+					swap(arrInsertion[j - 1], arrInsertion[j]);
+					++count;
+
+				}
+
+
+			arrTime[1] = count;
+
+			if (SIZE <= 1000) {
+
+				cout << "   Отсортированный массив : ";
+
+				for (int i = 0; i < SIZE; ++i) cout << arrInsertion[i] << " ";
+
+			}
+
+			cout << endl << " Время работы сортировки : " << count;
+
+			count = 0;
+
+			cout << endl << endl;
+
+			delete[]arrInsertion;
+
+		}
+		else cout << endl << " Так же долговато " << endl << endl;
+
+	}
+
