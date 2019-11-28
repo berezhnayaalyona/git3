87eed56c912bfa2762e11ed215adbbb380b109f7
berezhnayaalyona <berezhnayaa2015@mail.ru>
2019-11-28 14:33:52 +0300

    bubleSort

---
+++ cod1.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+#include <ctime>
+
+
+using namespace std;
+
+
+class Sorts {
+
+public:
+
+	void SetArr(int size) {
+
+		arr = new int[size];
+		this->SIZE = size;
+
+		int guess;
+		cout << " В каком порядке желаете заполнить массив ?\n 0 - В хаотичном\n 1 - В обычном\n 2 - В обратном\n : ";
+		cin >> guess;
+
+		cout << endl << "   Исходный массив :  ";
+
+		switch (guess) {
+
+		case 0: {
+
+			for (int i = 0; i < size; ++i) {
+
+				arr[i] = rand() % size + 1;
+				if (SIZE <= 2000)  cout << arr[i] << " ";
+
+			}
+
+			cout << endl << endl << " \n \tМассив заполнен в хаотичном порядке\n \n" << endl;
+			break;
+
+		}
+
+		case 1: {
+
+			for (int i = 0; i < size; ++i) {
+
+				arr[i] = i + 1;
+				if (SIZE <= 2000)  cout << arr[i] << " ";
+
+			}
+
+			cout << endl << endl << "  \n \tМассив заполнен в обычном порядке\n\n " << endl;
+			break;
+
+		}
+
+		case 2: {
+
+			for (int i = 0; i < size; ++i) {
+
+				arr[i] = size - i;
+				if (SIZE <= 2000)  cout << arr[i] << " ";
+
+			}
+
+			cout << endl << endl << "   \n\tМассив заполнен в обратном порядке\n\n " << endl;
+			break;
+
+		}
+
+		}
+
+	}
+
+	void bubbleSort() {
+
+		cout << " \t\tСортировка пузырьком\t\t\n " << endl;
+
+		if (SIZE <= 49999) {
+
+			int* arrBubble = new int[SIZE];
+
+
+			for (int i = 0; i < SIZE; ++i)	arrBubble[i] = arr[i];
+
+
+			for (int i = SIZE - 1; i > 0; --i, ++count) {
+
+				for (int j = 0; j < i; ++j, ++count) {
+
+					if (arrBubble[j] > arrBubble[j + 1] && ++count) {
+
+						swap(arrBubble[j], arrBubble[j + 1]);
+						++count;
+
+					}
+
+				}
+
+			}
+
+
+			arrTime[0] = count;
+
+			if (SIZE <= 1000) {
+
+				cout << "   Отсортированный массив : ";
+
+				for (int i = 0; i < SIZE; ++i) cout << arrBubble[i] << " ";
+
+			}
+
+			cout << endl << " Время работы сортировки : " << count;
+
+			count = 0;
+
+			cout << endl << endl;
+
+			delete[]arrBubble;
+
+		}
+
+		else cout << endl << " Устанем ждать " << endl << endl;
+
+	}
